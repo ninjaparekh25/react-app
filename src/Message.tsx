103045c1661f04d881d31d339ccd4b83eb71b43a
MeetParekh05 <meetbscit@gmail.com>
2025-01-18 11:30:13 +0530

    add

---
+++ src/Message.tsx
@@ -0,0 +1,14 @@
+
+// PascalCasing 
+function Message(){
+    //JSX: JavaScript XML
+    const name = "Meet"
+    if (name)
+        return <h1>Hello {name}</h1>
+    return <h1>Hello World!</h1>
+
+}
+
+export default Message;
+
+//21:46 video code with mosh
