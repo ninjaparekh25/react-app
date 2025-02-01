1fd0ae659d9607269cfbce68c9b995b819af5e38
MeetParekh05 <meetbscit@gmail.com>
2025-02-02 04:36:59 +0530

    add

--- assingment/chat-app/frontend/src/App.jsx
+++ assingment/chat-app/frontend/src/App.jsx
@@ -1,12 +1,27 @@
-import React from 'react'
+import Navbar from "./components/Navbar.jsx";
+
+import HomePage from "./pages/HomePage.jsx";
+import SignUpPage from "./pages/SignUpPage.jsx";
+import LoginPage from "./pages/LoginPage.jsx";
+import SettingsPage from "./pages/SettingsPage.jsx";
+import ProfilePage from "./pages/ProfilePage.jsx";
+
+import { Routes, Route } from "react-router-dom";
 
 const App = () => {
   return (
-    <div className="text-red-500">
-      hello
+    <div>
+      <Navbar />
+
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/signup" element={<SignUpPage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/setting" element={<SettingsPage />} />
+        <Route path="/profile" element={<ProfilePage />} />
+      </Routes>
     </div>
-  )
-}
+  );
+};
 
-export default App
-
+export default App;
