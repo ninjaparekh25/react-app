d8d4b5c585db8f6f37576506e26fa43c754fd585
MeetParekh05 <meetbscit@gmail.com>
2025-02-01 15:48:03 +0530

    add

--- assingment/chat-app/backend/src/controllers/auth.controller.js
+++ assingment/chat-app/backend/src/controllers/auth.controller.js
@@ -1,5 +1,37 @@
-export const signup = (req, res) => {
-    res.send("sigup route");
+import User from "../models/user.model.js"
+import bcrypt from "bcryptjs"
+
+export const signup = async(req, res) => {
+    const {fullName,email,password} = req.body;
+    try {
+        if (password.lenngth < 6) {
+            return res.status(400).json({message: "Password must be at least 6 characters"});
+        }
+
+        const user = await User.findOne({email})
+
+        if (user) return res.status(400).json({message:"email already exist"});
+
+        const salt = await bcrypt.genSalt(10)
+        const hashedPassword = await bcrypt.hash(password, salt)
+
+        const newUser = new User({
+            fullName: fullName,
+            email: email,
+            password: hashedPassword,
+        })
+
+        if(newUser) {
+            
+
+        } else {
+            res.status(400).json({ message:"Invaild user data" });
+        }
+
+
+    } catch (error) {
+        
+    }
 };
 
 export const login = (req, res) => {
