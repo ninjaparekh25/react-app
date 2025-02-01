916e59d78fb30af3b13ae9bdd8f733b5d649c88c
MeetParekh05 <meetbscit@gmail.com>
2025-02-01 17:30:25 +0530

    ADD

--- assingment/chat-app/backend/src/index.js
+++ assingment/chat-app/backend/src/index.js
@@ -1,5 +1,6 @@
 import express from "express"
 import dotenv from "dotenv"
+import cookieParser from "cookie-parser"
 
 import {connectDB} from "./lib/db.js"
 
@@ -11,8 +12,9 @@ const app = express();
 const PORT = process.env.PORT
 
 app.use(express.json());
+app.use(cookieParser());
 
-app.use("/api/auth", authRoutes)
+app.use("/api/auth", authRoutes);
 
 app.listen(PORT, () => {
     console.log("server is running on PORT: " + PORT);
