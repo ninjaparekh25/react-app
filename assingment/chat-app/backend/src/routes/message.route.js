59b44b788cf87dc165911a583e20943a5f0f3f9a
MeetParekh05 <meetbscit@gmail.com>
2025-02-02 03:08:18 +0530

    messageroutedone

--- assingment/chat-app/backend/src/routes/message.route.js
+++ assingment/chat-app/backend/src/routes/message.route.js
@@ -1,9 +1,12 @@
 import express from "express";
-import { protectRoute } from "../middleware/auth.middleware";
-import { getUsersForSidebar } from "../controllers/message.controller";
+import { protectRoute } from "../middleware/auth.middleware.js";
+import { getMessages, getUsersForSidebar, sendMessage } from "../controllers/message.controller.js";
 
 const router = express.Router();
 
 router.get("/users", protectRoute, getUsersForSidebar);
+router.get("/:id", protectRoute,getMessages);
+
+router.post("/send/:id",protectRoute, sendMessage);
 
 export default router;
