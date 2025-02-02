91af04b947fa2634d951745a4f6c1072c8c1703c
MeetParekh05 <meetbscit@gmail.com>
2025-02-02 23:29:26 +0530

    add

--- assingment/chat-app/frontend/src/App.jsx
+++ assingment/chat-app/frontend/src/App.jsx
@@ -14,7 +14,7 @@ import { Loader } from "lucide-react";
 import { Toaster } from "react-hot-toast";
 
 const App = () => {
-  const { authUser, checkAuth, isCheckingAuth, onlineUsers } = useAuthStore();
+  const { authUser, checkAuth, isCheckingAuth} = useAuthStore();
 
   useEffect(() => {
     checkAuth();
@@ -22,7 +22,7 @@ const App = () => {
 
   console.log({ authUser });
 
-  if (isCheckingAuth && !authUser)
+  if (isCheckingAuth && authUser)
     return (
       <div className="flex items-center justify-center h-screen">
         <Loader className="size-10 animate-spin" />
@@ -35,8 +35,8 @@ const App = () => {
 
       <Routes>
         <Route path="/" element={authUser ? <HomePage /> : <Navigate to="/login" />} />
-        <Route path="/signup" element={!authUser ? <SignUpPage /> : <Navigate to="/" />} />
-        <Route path="/login" element={!authUser ? <LoginPage /> : <Navigate to="/" />} />
+        <Route path="/signup" element={<SignUpPage /> } />
+        <Route path="/login" element={authUser ? <LoginPage /> : <Navigate to="/" />} />
         <Route path="/settings" element={<SettingsPage />} />
         <Route path="/profile" element={authUser ? <ProfilePage /> : <Navigate to="/login" />} />
       </Routes>
